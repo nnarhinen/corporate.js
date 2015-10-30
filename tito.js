a22a1a1ada6c7765f7cda05b371a2039b0c70b68
Niklas Närhinen <niklas@narhinen.net>
2015-10-31 01:02:38 +0200

    Also parse entries

--- tito.js
+++ tito.js
@@ -5,33 +5,59 @@ var XRegExp = require('xregexp'),
     lazy = require('lazy.js');
 
 
-    var basicInfoRe = new XRegExp('^T00' +
-                                  '(?<infoLength>                     .     {3}  )' +
-                                  '(?<version>                        .     {3}  )' +
-                                  '(?<bban>                           .     {14} )' +
-                                  '(?<statementNumber>                [0-9] {3}  )' +
-                                  '(?<startDate>                      [0-9] {6}  )' +
-                                  '(?<endDate>                        [0-9] {6}  )' +
-                                  '(?<creationDate>                   [0-9] {6}  )' +
-                                  '(?<creationTime>                   [0-9] {4}  )' +
-                                  '(?<contractId>                     .     {17} )' +
-                                  '(?<devnull>                        .     {31} )' +
-                                  '(?<currency>                       .     {3}  )' +
-                                  '(?<accountName>                    .     {30} )' +
-                                  '(?<devnull2>                       .     {18} )' +
-                                  '(?<accountOwnerName>               .     {35} )' +
-                                  '(?<bankContactInformation>        .     {40} )' +
-                                  '(?<bankContactInformationDetails> .     {40} )' +
-                                  '(?<devnull3>                       .     {30} )' +
-                                  '(?<iban>                           .     {18} ) \\ ' +
-                                  '(?<bic>                            .     {8}  )', 'x');
+var basicInfoRe = new XRegExp('^T00' +
+                              '(?<infoLength>                     .     {3}  )' +
+                              '(?<version>                        .     {3}  )' +
+                              '(?<bban>                           .     {14} )' +
+                              '(?<statementNumber>                [0-9] {3}  )' +
+                              '(?<startDate>                      [0-9] {6}  )' +
+                              '(?<endDate>                        [0-9] {6}  )' +
+                              '(?<creationDate>                   [0-9] {6}  )' +
+                              '(?<creationTime>                   [0-9] {4}  )' +
+                              '(?<contractId>                     .     {17} )' +
+                              '(?<devnull>                        .     {31} )' +
+                              '(?<currency>                       .     {3}  )' +
+                              '(?<accountName>                    .     {30} )' +
+                              '(?<devnull2>                       .     {18} )' +
+                              '(?<accountOwnerName>               .     {35} )' +
+                              '(?<bankContactInformation>        .     {40} )' +
+                              '(?<bankContactInformationDetails> .     {40} )' +
+                              '(?<devnull3>                       .     {30} )' +
+                              '(?<iban>                           .     {18} ) \\ ' +
+                              '(?<bic>                            .     {8}  )', 'x');
 
+var entryInfoRe = new XRegExp('^T10' +
+                              '(?<infoLength>         .     {3}  )' +
+                              '(?<entryNumber>        [0-9] {6}  )' +
+                              '(?<archiveId>          .     {18} )' +
+                              '(?<bookingDate>        [0-9] {6}  )' +
+                              '(?<valueDate>          [0-9] {6}  )' +
+                              '(?<paymentDate>        [0-9] {6}  ) (.) ' +
+                              '(?<bookingInformation> .     {38} )' +
+                              '(?<sign>               .     {1}  )' +
+                              '(?<amount>             [0-9] {18} )' +
+                              '(?<devnull>            .     {2}  )' +
+                              '(?<receiver>           .     {35} ) (.)' +
+                              '(?<receiverBban>       .     {14} ) (.)' +
+                              '(?<referenceNumber>    .     {20} )', 'x');
+
+var additionalEntryInfoRe = new XRegExp('^T11' +
+                                        '(?<infoLength> [0-9] {3} )' +
+                                        '(?<infoType>   .     {2} )', 'x');
 
 function parseTito(txt) {
-  var headerInfo;
-  txt.split("\n").map(function(line) {
+  var headerInfo,
+      entries = [];
+  txt.split("\n").forEach(function(line) {
+    line = line.replace(/\}/g, 'å')
+               .replace(/{/g, 'ä')
+               .replace(/\|/g, 'ö')
+               .replace(/\]/g, 'Å')
+               .replace(/\[/g, 'Ä')
+               .replace(/\\/g, 'Ö');
+    var m;
     if (basicInfoRe.test(line)) {
-      var m = XRegExp.exec(line, basicInfoRe);
+      m = XRegExp.exec(line, basicInfoRe);
       headerInfo = {
         accountName: m.accountName.trim(),
         accountOwnerName: m.accountOwnerName.trim(),
@@ -48,14 +74,40 @@ function parseTito(txt) {
         startDate: stringToDateString(m.startDate),
         statementNumber: m.statementNumber
       };
+    } else if (entryInfoRe.test(line)) {
+      m = XRegExp.exec(line, entryInfoRe);
+      entries.push({
+        additionalInformation: [],
+        amount: Number(m.amount) / 100 * (m.sign === '-' ? -1 : 1),
+        archiveId: m.archiveId.trim(),
+        bookingDate: stringToDateString(m.bookingDate),
+        bookingInformation: m.bookingInformation.trim(),
+        paymentDate: stringToDateString(m.paymentDate),
+        receiver: m.receiver.trim(),
+        receiverBban: m.receiverBban.trim(),
+        referenceNumber: m.referenceNumber.trim() ? parseInt(m.referenceNumber, 10) + '' : '',
+        valueDate: stringToDateString(m.valueDate)
+      });
+    } else if (additionalEntryInfoRe.test(line)) {
+      m = XRegExp.exec(line, additionalEntryInfoRe);
+      var entry = entries[entries.length - 1];
+      if (['00', '07', '06', '08'].indexOf(m.infoType) !== -1 ) {
+        var totalLength = Number(m.infoLength), parts = [];
+        for (var i = 8; i < totalLength; i=i+35) {
+          parts.push(line.substring(i, i+35).trim());
+        }
+        entry.additionalInformation.push(parts.join(' '));
+      }
     }
   });
   return {
-    header: headerInfo
+    header: headerInfo,
+    entries: entries
   };
 }
 
 function stringToDateString(str) {
+  if (str === '000000') return '0000-00-00';
   var parts = str.match(/.{1,2}/g);
   return '20' + parts[0] + '-' + parts[1] + '-' + parts[2];
 }
